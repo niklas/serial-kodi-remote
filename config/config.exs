e03beef1c942ab9784ebc5d025ab9ebf72a43264
Niklas Hofer <niklas+dev@lanpartei.de>
2019-11-13 17:51:48 +0100

    Use Elixir 1.9's new config.
    Toml is still ignored.

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
@@ -21,7 +21,7 @@ use Mix.Config
 #     config :logger, level: :info
 #
 config :serial_kodi_remote,
-  kodi_ws_url: "ws://login:password@host:9090/jsonrpc",
+  kodi_ws_url: "ws://login:password@kodihost:9090/jsonrpc",
   serial_port: "/dev/ttyUSB0"
 
 # It is also possible to import configuration files, relative to this
