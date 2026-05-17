e8544f9d461b8cec58e3f5f0559e389a597ad784
Niklas Hofer <niklas+dev@lanpartei.de>
2026-05-17 18:06:18 +0200

    use Layouts.app

--- lib/serial_kodi_remote_web/components/layouts.ex
+++ lib/serial_kodi_remote_web/components/layouts.ex
@@ -38,25 +38,20 @@ defmodule SerialKodiRemoteWeb.Layouts do
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+          SKR
+          <span class="text-sm font-semibold">v{Application.spec(:serial_kodi_remote, :vsn)}</span>
         </a>
       </div>
       <div class="flex-none">
         <ul class="flex flex-column px-1 space-x-4 items-center">
           <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
+            <a href="https://git.lanpartei.de/niklas/serialkodiremote" class="btn btn-ghost">Gitlab</a>
           </li>
           <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
+            <a href="https://github.com/niklas/serial-kodi-remote" class="btn btn-ghost">Github</a>
           </li>
           <li>
             <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
           </li>
         </ul>
       </div>
